76156fe6a906b6e1c963efbc52a8caff45e02ee0
likai79511 <pruenature@163.com>
2017-12-19 23:31:33 +0800

    map function

--- app/src/main/java/com/agera/hometools/locate/LocateActivity.kt
+++ app/src/main/java/com/agera/hometools/locate/LocateActivity.kt
@@ -1,23 +1,24 @@
 package com.agera.hometools.locate
 
 import android.app.Activity
-import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import com.agera.hometools.R
 import com.amap.api.maps.AMap
 import com.amap.api.maps.CameraUpdateFactory
 import com.amap.api.maps.MapView
+import com.amap.api.maps.model.LatLng
 import com.amap.api.maps.model.MyLocationStyle
 
 /**
  * Created by Agera on 2017/11/8.
  */
 class LocateActivity : Activity() {
-    var mMap: MapView? = null
-    var mMapControl: AMap? = null
+    private var mMap: MapView? = null
+    private var mMapControl: AMap? = null
 
-    var locationStyle: MyLocationStyle = MyLocationStyle()
+    private var locationStyle: MyLocationStyle = MyLocationStyle()
+    private var isShowInCenter = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +27,32 @@ class LocateActivity : Activity() {
         initView(savedInstanceState)
         locationStyle.interval(2500)
         locationStyle.showMyLocation(true)
+        locationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER)
+
     }
 
-    fun initView(savedInstanceState: Bundle?) {
+    private fun initView(savedInstanceState: Bundle?) {
         mMap = findViewById(R.id.map) as MapView?
         mMap!!.onCreate(savedInstanceState)
         if (mMap != null)
             mMapControl = mMap!!.map
         initMap(true)
         mMapControl?.moveCamera(CameraUpdateFactory.zoomTo(14f))
-
+        mMapControl?.isTrafficEnabled = true
+        mMapControl?.uiSettings?.isZoomControlsEnabled = false
+        mMapControl?.uiSettings?.isMyLocationButtonEnabled = true
+        mMapControl?.setOnMyLocationChangeListener {
+            if (isShowInCenter) {
+                mMapControl?.moveCamera(CameraUpdateFactory.changeLatLng(LatLng(it.latitude, it.longitude)))
+                isShowInCenter = false
+            }
+        }
     }
 
-    fun initMap(flag:Boolean) {
+    private fun initMap(flag: Boolean) {
         mMapControl!!.myLocationStyle = locationStyle
         mMapControl!!.isMyLocationEnabled = flag
     }
-
 
 
     override fun onDestroy() {
@@ -60,7 +70,7 @@ class LocateActivity : Activity() {
         super.onPause()
         mMap!!.onPause()
         initMap(false)
-        Log.e("---","---map onPause")
+        Log.e("---", "---map onPause")
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
